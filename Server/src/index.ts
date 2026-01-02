ec2ccaad70392b0d38ecd84e9f4f7e93e095aade
BLITzZ0 <ababhishek3005@gmail.com>
2026-01-02 21:45:32 +0530

    2janupdates

---
+++ Server/src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+const app = express()
+const port = 3000
+
+app.get("/",(req,res)=>{
+    console.log("Hello World");
+    res.send("hello");
+});
+
+app.listen(port,()=>{
+    console.log("App is running in port: " + port)
+})
